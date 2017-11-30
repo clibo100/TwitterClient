232e2d44f56e9e12fa503c8cf1b1e1b406aeb52d
Sierra Clibourne <clibo100@mail.chapman.edu>
2017-11-29 19:03:52 -0800

    more gui integration

--- test.py
+++ test.py
@@ -8,8 +8,7 @@ twitter_api = twitter.Api(consumer_key='d8ICLBVp1RE6hEIuohrNUyCGT',
                       consumer_secret='Ri4GQ1eEc8yLwpkCI4UkKxtbMXxObLzor8Gep97pXHROlLF9WR',
                       access_token_key='908004742424518656-ePYoPahlW08jOWm62ppI9U6FOqUOEeN',
                       access_token_secret='5nP7uYGnnE5qo2sYKKpYkQ4QDQJ9oEB7RTOWoLabFC5cc')
-global buttonswitch 
-buttonswitch = 0
+
 global counter
 counter = 0
 
@@ -25,15 +24,14 @@ class TwitterClient:
 
         self.searchbar = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
         self.searchbutton = Button(master, text="Search", command = self.greet)
-        self.mytweets = Button(master, text="My Tweets", command = self.greet)
-        self.timeline = Button(master, text="Timeline", command = self.greet)
-        self.followers = Button(master, text="Followers", command = self.clickFollowers)
+        self.mytweets = Button(master, text="My Tweets", command = self.clickMyTweets)
+        self.timeline = Button(master, text="Timeline", command = self.clickTimeline)
+        self.followers = Button(master, text="Following", command = self.clickFollowing)
         self.tweet = Button(master, text="Tweet", command = self.greet)
         self.name = Label(master, text="Barack Obama")
         self.user = Label(master, text="@BarackObama")
-        self.body = Label(master, text="body is empty")
-        self.next = Button(master, text="Next", command = self.clickNext(buttonswitch, counter))
-
+        self.body = Label(master, text="body is empty", wraplength = 200)
+        self.next = Button(master, text="Next", command = self.greet)
 
         self.searchbar.grid(row = 0, column = 0, columnspan = 10, sticky = W)
         self.searchbutton.grid(row = 0, column = 11, columnspan = 10, sticky = W)
@@ -65,24 +63,42 @@ class TwitterClient:
     def tweet(self, text):
         print text
 
+    def printMyTweet(self, counter):
+        statuses = twitter_api.GetUserTimeline(908004742424518656) #this is the ID of @clibo100, once we impliment loggin we need to change this to the user ID of the user in question
+        self.name.configure(text = statuses[counter].user.name)
+        self.user.configure(text = "@" + statuses[counter].user.screen_name)
+        self.body.configure(text = statuses[counter].text)
+        counter += 1
+
+    def printTimelineTweet(self, counter):
+        statuses = twitter_api.GetHomeTimeline()
+        self.name.configure(text = statuses[counter].user.name)
+        self.user.configure(text = "@" + statuses[counter].user.screen_name)
+        self.body.configure(text = statuses[counter].text)
+        counter += 1
+
     def printFollowing(self, counter):
         users = twitter_api.GetFriends()
         self.name.configure(text = users[counter].name)
         self.user.configure(text = "@" + users[counter].screen_name)
         counter += 1
 
-    def clickNext(self, buttonswitch, counter): #TODO: next button doesn't work so yeah fix that
-        if buttonswitch == 2:
-            self.printFollowing(counter)
-
-    def clickFollowers(self):
+    def clickFollowing(self):
         counter = 0
-        buttonswitch = 2
         self.body.configure(text = " ")
-        self.clickNext(buttonswitch, counter)
+        self.next.configure(command = self.printFollowing(counter))
+        self.printFollowing(counter)
+        counter += 1
 
+    def clickMyTweets(self):
+        counter = 0
+        self.printMyTweet(counter)
+        counter += 1
 
-
+    def clickTimeline(self):
+        counter = 0
+        self.printTimelineTweet(counter)
+        counter += 1
 
 root = Tk()
 twitter_gui = TwitterClient(root)
